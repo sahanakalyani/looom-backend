ece5aefc891e783051fd2a7d2e23b44b8c737230
sahanakalyani <sahanakalyani96@gmail.com>
2026-02-04 12:27:55 +0530

    sql

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import "dotenv/config";
 import express from "express";
+import { pool } from "./db.js";
 
 const PORT = process.env.PORT || 3000;
 
@@ -10,11 +11,14 @@ app.get("/", (req, res) => {
 
 async function startServer() {
   try {
+    await pool.query("SELECT 1");
+    console.log("Data connection verified");
+
     app.listen(PORT, () => {
       console.log(`server is running on :http://Localhost:${PORT}`);
     });
   } catch (err) {
-    console.error("err");
+    console.error(err);
   }
 }
 startServer();
