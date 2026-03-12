0fec53850cc7d1425ca2f7e3549e0d23f410a50c
sahanakalyani <sahanakalyani96@gmail.com>
2026-03-12 14:48:51 +0530

    appdevelopment

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { getUserProfile } from "../controllers/user.controller.js";
+import { optionalAuth } from "../middleware/auth.js";
 
 const route = express.Router();
-route.get("/:id", getUserProfile);
+route.get("/:id", optionalAuth, getUserProfile);
 export default route;
